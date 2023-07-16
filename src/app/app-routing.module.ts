2c49386f8ec4ea0af870aa3d2f62fc8cc3c0ce15
Rafael Kitoco <rkitoco@gmail.com>
2023-07-16 13:27:50 +0100

    novo repositorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"nav", component:NavbarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
